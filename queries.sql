4bc0413530a5cd0ff3de14992fb92f7e3551b34b
TALEX1995 <tanase.alex@libero.it>
2023-07-28 16:33:50 +0200

    Query group 1

--- queries.sql
+++ queries.sql
@@ -26,6 +26,8 @@ SELECT * FROM `teachers` WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(*) AS `enrolment_for_year`, YEAR(`enrolment_date`) AS `enrolment_year` FROM `students` GROUP BY `enrolment_year`;
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
